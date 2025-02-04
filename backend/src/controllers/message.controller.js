e648e19de5a0224107d12dd49b4a6123a944ff56
sombit-k <sombitkarmakar018@gmail.com>
2025-02-04 21:43:32 +0530

    final push

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js"
-import Message from "../models/Message.model.js"
+import Message from "../models/message.model.js"
 import cloudinary from "../lib/cloudinary.js"
 import { getReceiverSocketId,io } from "../lib/socket.js"
 
